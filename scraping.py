27208379882366ca2818be0c6e5d3397252d9d0c
kasatomorning <9549356+kasatomorning@users.noreply.github.com>
2023-09-25 13:02:51 +0900

    spreadsheetdataのリストのdiffを取る関数を作成

--- scraping.py
+++ scraping.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import requests
 import mysql.connector
 from mysql.connector import Error
@@ -6,6 +7,8 @@ from enum import Enum
 import pprint
 import os
 from dotenv import load_dotenv
+import copy
+
 
 target_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRmmWiBmMMD43m5VtZq54nKlmj0ZtythsA1qCpegwx-iRptx2HEsG0T3cQlG1r2AIiKxBWnaurJZQ9Q/pubhtml#"
 
@@ -24,7 +27,7 @@ class SpreadsheetData:
     first_name: str
     family_name: str
     end_date: int
-    is_resident: bool
+    resident: str
     roster_status: str
     team_tag: str
     team_contact_info: str
@@ -38,7 +41,7 @@ class SpreadsheetData:
         first_name: str,
         family_name: str,
         end_date: str,
-        resident_status: bool,
+        resident: str,
         roster_status: str,
         team_tag: str,
         team_contact_info: str,
@@ -50,10 +53,29 @@ class SpreadsheetData:
         self.first_name = first_name
         self.family_name = family_name
         self.end_date = int(end_date) if end_date != "" else 0
-        self.is_resident = resident_status == "Resident"
+        # resident_statusは、boolか"Resident"かそれ以外の文字列のいずれか
+        self.resident = resident
         self.roster_status = roster_status
         self.team_tag = team_tag
         self.team_contact_info = team_contact_info
+
+    def __eq__(self, other: SpreadsheetData):
+        return (
+            self.league == other.league
+            and self.team_name == other.team_name
+            and self.handle_name == other.handle_name
+            and self.role == other.role
+            and self.first_name == other.first_name
+            and self.family_name == other.family_name
+            and self.end_date == other.end_date
+            and self.resident == other.resident
+            and self.roster_status == other.roster_status
+            and self.team_tag == other.team_tag
+            and self.team_contact_info == other.team_contact_info
+        )
+
+    def __ne__(self, other: SpreadsheetData):
+        return not self.__eq__(other)
 
     def values(self):
         return [
@@ -64,7 +86,7 @@ class SpreadsheetData:
             self.first_name,
             self.family_name,
             self.end_date,
-            self.is_resident,
+            self.resident,
             self.roster_status,
             self.team_tag,
             self.team_contact_info,
@@ -107,21 +129,18 @@ def connect_to_mysql_server(host_name, user_name, user_password):
 
 
 def create_or_check_database(connection, db_name):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(
-            "CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'utf8'".format(
-                db_name
-            )
-        )
-        print("Database created successfully or already exists")
-    except Error as err:
-        print("Failed creating database: {}".format(err))
-        exit(1)
+    query = "CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'utf8'".format(
+        db_name
+    )
+    execute_query(
+        connection,
+        query,
+        success_message="Create DB or already exists",
+        error_message="Failed creating database",
+    )
 
 
 def create_or_check_table(connection, table_name):
-    cursor = connection.cursor()
     query = """
     CREATE TABLE IF NOT EXISTS {} (
         league VARCHAR(50),
@@ -131,7 +150,7 @@ def create_or_check_table(connection, table_name):
         first_name VARCHAR(50) NOT NULL,
         family_name VARCHAR(50) NOT NULL,
         end_date INT(4) NOT NULL,
-        is_resident BOOLEAN,
+        resident VARCHAR(50),
         roster_status VARCHAR(50),
         team_tag VARCHAR(50),
         team_contact_info VARCHAR(50),
@@ -140,42 +159,98 @@ def create_or_check_table(connection, table_name):
     """.format(
         table_name
     )
-    try:
-        cursor.execute(query)
-        print("Table created successfully or already exists")
-    except Error as err:
-        print("Failed creating table: {}".format(err))
-        exit(1)
+    execute_query(
+        connection,
+        query,
+        success_message="Create table or already exists",
+        error_message="Failed creating table",
+    )
 
 
-def execute_query(connection, query):
+def execute_query(
+    connection, query, success_message: str = None, error_message: str = "Error"
+):
     cursor = connection.cursor()
     try:
         cursor.execute(query)
         connection.commit()
-        print("Query executed successfully")
+        if success_message is not None:
+            print(success_message)
+    except Error as err:
+        print("{}: '{}'".format(error_message, err))
+        exit(1)
+
+
+def write_data_to_db(connection, table_name, data_list):
+    cursor = connection.cursor()
+    query = """ INSERT INTO `{}`(
+        `league`, `team_name`, `handle_name`,`role`,`first_name`,`family_name`,
+        `end_date`,`resident`,`roster_status`,`team_tag`,`team_contact_info`)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """.format(
+        table_name
+    )
+    try:
+        cursor.executemany(query, [data.values() for data in data_list])
+        connection.commit()
+        print("Success writing table")
     except Error as err:
         print("Error: '{}'".format(err))
         exit(1)
 
 
-def write_data_to_table(connection, table_name, data_list):
-    cursor = connection.cursor()
-    query = """ INSERT INTO `{}`(
-        `league`, `team_name`, `handle_name`,`role`,`first_name`,`family_name`,
-        `end_date`,`is_resident`,`roster_status`,`team_tag`,`team_contact_info`)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """.format(
-        table_name
-    )
-    # cursor.executemany(query, data_list)
-    cursor.executemany(query, [data.values() for data in data_list])
-    connection.commit()
-
-
-def read_data_from_table(connection, table_name):
+def read_data_from_db(connection, TABLE_NAME):
     # TODO: テーブルのデータを読み込む
-    pass
+    cursor = connection.cursor()
+    query = "SELECT * FROM {}".format(TABLE_NAME)
+    try:
+        cursor.execute(query)
+        data_list_from_db = [SpreadsheetData(*row) for row in cursor.fetchall()]
+        print("Success reading table")
+        return data_list_from_db
+    except Error as err:
+        print("Failed reading table: '{}'".format(err))
+        exit(1)
+
+
+def show_data_list(data_list):
+    if data_list == []:
+        print("No data in this list")
+        return
+    for data in data_list:
+        print(data.values())
+
+
+def is_same_data_list(
+    data_list_new: list[SpreadsheetData], data_list_old: list[SpreadsheetData]
+):
+    if data_list_new == [] and data_list_old == []:
+        print("No data in either list")
+        return False
+    is_same = True
+    # それぞれのリストをfirst_nameでソート
+    data_list_new.sort(key=lambda x: x.first_name)
+    data_list_old.sort(key=lambda x: x.first_name)
+    data_list_new_diff = copy.deepcopy(data_list_new)
+    data_list_old_diff = copy.deepcopy(data_list_old)
+    for new_data in data_list_new:
+        for old_data in data_list_old:
+            # もし同じ名前のデータがあったら
+            if (
+                new_data.first_name == old_data.first_name
+                and new_data.family_name == old_data.family_name
+            ):
+                # それぞれのデータを比較
+                if new_data != old_data:
+                    is_same = False
+                    print("{}→{}".format(old_data.values(), new_data.values()))
+                data_list_new_diff.remove(new_data)
+                data_list_old_diff.remove(old_data)
+    for new_data in data_list_new_diff:
+        print("[]=>{}".format(new_data.values()))
+    for old_data in data_list_old_diff:
+        print("{}=>[]".format(old_data.values()))
+    return is_same
 
 
 def main():
@@ -196,12 +271,18 @@ def main():
     create_or_check_database(connection, DB_NAME)
     # DBを選択
     execute_query(connection, "USE {}".format(DB_NAME))
-    # テーブルを作成
+    # テーブルを作成|存在確認
     create_or_check_table(connection, TABLE_NAME)
+
     # テーブルのデータを表示
-    pprint.pprint([d.values() for d in data_list_from_spreadsheet], width=300)
+    data_list_from_db = read_data_from_db(connection, TABLE_NAME)
+
+    is_same_data_list(data_list_from_spreadsheet, data_list_from_db)
+    # show_data_list(data_list_from_db)
     # テーブルにデータを書き込み
-    write_data_to_table(connection, TABLE_NAME, data_list_from_spreadsheet)
+    # write_data_to_db(connection, TABLE_NAME, data_list_from_spreadsheet)
+    # show_data_list(data_list_from_spreadsheet)
+
     # MySQLサーバーとの接続を切断
     connection.close()
 
