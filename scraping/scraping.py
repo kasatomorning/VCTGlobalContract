5b0c11c13694219e91c97195b4632a66f3db6d6d
kasatomorning <9549356+kasatomorning@users.noreply.github.com>
2024-09-06 00:13:54 +0900

    utilsにまとめた

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from model.models import SpreadsheetData, League
-from log.logger import setup_logger
+from utils.utils import setup_logger
 
 logger = setup_logger(__name__)
 
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,14 @@
 import unicodedata
+from logging import getLogger, StreamHandler, DEBUG
+
+
+def setup_logger(name):
+    logger = getLogger(name)
+    logger.setLevel(DEBUG)
+    console_handler = StreamHandler()
+    console_handler.setLevel(DEBUG)
+    logger.addHandler(console_handler)
+    return logger
 
 
 def normalize_unicode(words: str) -> str:
