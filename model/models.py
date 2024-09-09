4834335cd79c7458c5057bd51b9336c84ea954b5
kasatomorning <9549356+kasatomorning@users.noreply.github.com>
2024-09-09 22:04:25 +0900

    chore: dataclassを使うように

--- model/models.py
+++ model/models.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from utils.utils import normalize_unicode
+from dataclasses import dataclass
 
 
 class League(Enum):
@@ -16,50 +17,24 @@ class Color(Enum):
     ADDED = 7935
 
 
+@dataclass
 class SpreadsheetData:
-    def __init__(
-        self,
-        league: League,
-        team_name: str,
-        handle_name: str,
-        role: str,
-        first_name: str,
-        family_name: str,
-        end_date: str,
-        resident: str,
-        roster_status: str,
-        team_tag: str,
-        team_contact_info: str,
-    ):
-        self.league = league
-        self.team_name = team_name
-        self.handle_name = handle_name
-        self.role = role
-        self.first_name = normalize_unicode(first_name)
-        self.family_name = normalize_unicode(family_name)
-        self.end_date = int(end_date) if end_date != "" else 0
-        self.resident = resident
-        self.roster_status = roster_status
-        self.team_tag = team_tag
-        self.team_contact_info = team_contact_info
+    league: League
+    team_name: str
+    handle_name: str
+    role: str
+    first_name: str
+    family_name: str
+    end_date: int
+    resident: str
+    roster_status: str
+    team_tag: str
+    team_contact_info: str
 
-    def __eq__(self, other: "SpreadsheetData"):
-        return (
-            self.league == other.league
-            and self.team_name == other.team_name
-            and self.handle_name == other.handle_name
-            and self.role == other.role
-            and self.first_name == other.first_name
-            and self.family_name == other.family_name
-            and self.end_date == other.end_date
-            and self.resident == other.resident
-            and self.roster_status == other.roster_status
-            and self.team_tag == other.team_tag
-            and self.team_contact_info == other.team_contact_info
-        )
-
-    def __ne__(self, other: "SpreadsheetData"):
-        return not self.__eq__(other)
+    def __post_init__(self):
+        self.first_name = normalize_unicode(self.first_name)
+        self.family_name = normalize_unicode(self.family_name)
+        self.end_date = int(self.end_date) if self.end_date != "" else 0
 
     def values(self):
         return [
